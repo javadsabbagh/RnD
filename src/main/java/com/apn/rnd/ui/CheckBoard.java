de123e76a98fc20e048883624a8d3622ad4682b1
javadsabbagh <j.sabbagh91@gmail.com>
2016-09-22 17:22:25 +0330

    Add properties to the CheckBoard

--- src/main/java/com/apn/rnd/ui/CheckBoard.java
+++ src/main/java/com/apn/rnd/ui/CheckBoard.java
@@ -26,26 +26,54 @@ public class CheckBoard extends Canvas{
         super(width, height);
         createBoard();
     }
+
+    public Paint getColor1() {
+        return color1;
+    }
+
+    public void setColor1(Paint color1) {
+        this.color1 = color1;
+        repaint();
+    }
+
+    public Paint getColor2() {
+        return color2;
+    }
+
+    public void setColor2(Paint color2) {
+        this.color2 = color2;
+        repaint();
+    }
+
+    public double getSquareSize() {
+        return SQURE_SIE;
+    }
+
+    public void setSquareSize(double squareSize) {
+        this.SQURE_SIE = squareSize;
+        repaint();
+    }           
+    
            
    private void createBoard() {
         this.widthProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                repaint(CheckBoard.this.getWidth(), CheckBoard.this.getHeight());
+                repaint();
             }
         });
 
         this.heightProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                repaint(CheckBoard.this.getWidth(), CheckBoard.this.getHeight());
+                repaint();
             }
         });
     }
     
-    private void repaint(double width, double height) {
-        cols = Math.ceil(width / SQURE_SIE);
-        rows = Math.ceil(height / SQURE_SIE);
+    private void repaint() {
+        cols = Math.ceil(this.getWidth() / SQURE_SIE);
+        rows = Math.ceil(this.getHeight() / SQURE_SIE);
 
         GraphicsContext gc = this.getGraphicsContext2D();
         Paint tempColor;
